6a1b05c85ebc0ac75a89b433bfa7e6216d8cdab8
manhcuongbui <72534968+manhcuongbui@users.noreply.github.com>
2020-11-13 20:59:07 +0700

    update

---
+++ đồ án 1/Business/Interface/ISanPhamBLL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace đồ_án_1.Business.Interface
+{
+    public interface ISanPhamBLL
+    {
+        List<SanPham> GetAllSanPham();
+        void ThemSanPham(SanPham sp);
+        void XoaSanPham(string masanpham);
+        void SuaSanPham(SanPham sp);
+        List<SanPham> TimSanPham(SanPham sp);
+    }
+}
---
+++ đồ án 1/DataAccessLayer/Interface/ISanPhamDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace đồ_án_1.DataAccessLayer.Interface
+{
+    class ISanPhamDAL
+    {
+        public interface ISanPhamDAL
+        {
+            List<SanPham> GetAllSanPham();
+            void ThemSanPham(SanPham hs);
+            void Update(List<SanPham> list);
+        }
+    }
+}
